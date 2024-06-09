27762174566ec4124f945fe88a1261a61f693086
Mohamed Rafik <71353030+BounaderMedRafik@users.noreply.github.com>
2024-06-09 17:09:06 +0100

    ss

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Search from "@/components/Search";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen w-full justify-center items-center">
+      <Search />
+    </main>
+  );
+}
